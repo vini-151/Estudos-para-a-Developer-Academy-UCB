5c58e8dc259f11e8c71ed602bddb6e5d27816fa7
Vini <ovini.15515@gmail.com>
2026-09-15 10:59:24 -0300

    fixes

--- Programação estruturada/batch 2/ponteiro.c
+++ Programação estruturada/batch 2/ponteiro.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
+    char *a = "abacate";
+    char *b = "uva";
 
-    char *a, *b;
-    a = "abacate";
-    b = "uva";
-
-        if(a < b)
-        printf("%i vem antes de %i no dicionário", a, b);
-    
+    if (strcmp(a, b) < 0)
+        printf("%s vem antes de %s no dicionário\n", a, b);
     else
-    
-        printf("%s vem depois de %s no dicionário", a, b);
-    
+        printf("%s vem depois de %s no dicionário\n", a, b);
+
+    return 0;
 }
